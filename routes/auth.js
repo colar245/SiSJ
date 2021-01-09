b7c474d3e03bf472ae9a0b5dfc2de7cd380c9b47
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2021-01-09 23:13:55 +0100

    navbar

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,21 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 
-// @desc Auth with Google
-// @route GET /auth/google
+// @desc    Auth with Google
+// @route   GET /auth/google
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
-// @desc Google auth callback
-// @route GET /auth/google/callback
+// @desc    Google auth callback
+// @route   GET /auth/google/callback
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (seq, res) => {
     res.redirect('/dashboard')
+})
+
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout', (req,res) => {
+    req.logout()
+    res.redirect('/')
 })
 
 module.exports = router

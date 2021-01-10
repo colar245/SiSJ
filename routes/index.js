e6d5ecf450562920f4400622b668548bb964e0a3
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2021-01-10 18:04:32 +0100

    add & display Notes; formate Date

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const { Store } = require('express-session')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const Story = require('../models/Story')
 
 // @desc Login/Landing page
 // @route GET /
@@ -10,12 +12,19 @@ router.get('/', ensureGuest, (req, res) => {
     })
 })
 
-// @desc Dashboard
-// @route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+      const stories = await Story.find({ user: req.user.id }).lean()
+      res.render('dashboard', {
         name: req.user.firstName,
-    })
-})
-
+        stories,
+      })
+    } catch (err) {
+      console.error(err)
+      res.render('error/500')
+    }
+  })
+  
 module.exports = router
